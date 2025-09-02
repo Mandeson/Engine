ab3814d598d4e518f798f7de68fa03cae94b309e
Tomasz Klimek <tomkl584@gmail.com>
2025-09-02 15:55:54 +0200

    Add subtraction operator to vector

--- src/util/Vector.hpp
+++ src/util/Vector.hpp
@@ -14,6 +14,10 @@ struct Vector2 {
         return Vector2<T>{static_cast<T>(x + other.x), static_cast<T>(y + other.y)};
     }
 
+    Vector2<T> operator-(const Vector2<T> &other) const {
+        return Vector2<T>{static_cast<T>(x - other.x), static_cast<T>(y - other.y)};
+    }
+
     template <typename S>
     operator Vector2<S>() const {
         return Vector2<S>{static_cast<S>(x), static_cast<S>(y)};
