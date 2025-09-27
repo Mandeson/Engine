84ee9a7daa9430e986f7625de1f19d0180b2abed
Tomasz Klimek <tomkl584@gmail.com>
2025-09-27 23:23:15 +0200

    Add division operator to Vector, add Vector2d alias

--- src/util/Vector.hpp
+++ src/util/Vector.hpp
@@ -9,6 +9,11 @@ struct Vector2 {
     Vector2<T> operator*(D val) const {
         return Vector2<T>{static_cast<T>(x * val), static_cast<T>(y * val)};
     }
+
+    template <typename D>
+    Vector2<T> operator/(D val) const {
+        return Vector2<T>{static_cast<T>(x / val), static_cast<T>(y / val)};
+    }
     
     Vector2<T> operator+(const Vector2<T> &other) const {
         return Vector2<T>{static_cast<T>(x + other.x), static_cast<T>(y + other.y)};
@@ -26,3 +31,4 @@ struct Vector2 {
 
 using Vector2i = Vector2<int>;
 using Vector2f = Vector2<float>;
+using Vector2d = Vector2<double>;
